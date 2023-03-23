656e59514afb33f6d3a5ac9ea8e9e9fbdf2b713c
iwabuchi yuki <yukibuchi17.keio.jp>
2023-03-23 17:48:30 +0900

    gpu compatible

--- make_container.sh
+++ make_container.sh
@@ -3,7 +3,7 @@
 # FileName: 	make_container
 # Author: 8ucchiman
 # CreatedDate:  2023-01-26 17:02:35 +0900
-# LastModified: 2023-03-23 16:05:10 +0900
+# LastModified: 2023-03-23 16:24:26 +0900
 # Reference: 8ucchiman.jp
 #
 
@@ -31,7 +31,14 @@ do
 done
 if [[ -z gpu_flag ]]
 then
-    docker run -it --gpus all $image_name
+    docker run -it --rm --gpus all \
+               --net host \
+               --env DISPLAY=$DISPLAY \
+               --volume $HOME/.Xauthority:/home/bucchiman/.Xauthority \
+               --volume $HOME/.config/snippets:/home/bucchiman/lib \
+               --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
+               --user="bucchiman" \
+               $image_name
 else
     docker run -it --rm \
                --net host \
